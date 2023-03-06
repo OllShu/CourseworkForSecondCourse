bf853434747f3b6ab7c051f17cbaddff55d131ee
dns <shulgaol@mail.ru>
2023-03-06 12:38:27 +0200

    курсовые

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+  //    typeNumberReceipt();
+    }
+   /*
+   public static int typeNumberReceipt() {
+
+
+        Scanner scanner = new Scanner(System.in);
+        int typeNumber = 0;
+        while (!scanner.hasNextInt()) {
+            System.out.println("Выберите тип задачи и введите цифру: 1 - рабочая, 2 - личная");
+            typeNumber = scanner.nextInt();
+            if (typeNumber != 1 && typeNumber != 2) {
+                System.out.println("Некорректный ввод. Выберите тип задачи и введите цифру: 1 - рабочая, 2 - личная");
+            }
+            break;
+        }
+        return typeNumber;
+    }
+
+    */
+}
